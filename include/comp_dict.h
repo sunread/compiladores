bc71d9a351ca97d572bc36055992c677038c09f4
fsoller <fsoller@gmail.com>
2013-09-16 04:05:59 -0300

    Update comp_dict.h

--- include/comp_dict.h
+++ include/comp_dict.h
@@ -1,15 +1,26 @@
 /* Projeto de Compiladores 2013-2 - Etapa 1
    Fernando Soller Mecking
-   Lucas Herbert Jones
    Mateus Cardoso da Silva
+
+   comp_dict.h
+   Estruturas de dados e definições do dicionário
 */
+
+union dataValue
+{
+    int i;
+    float f;
+    char c;
+    char *str;
+}dataValue;
 
 typedef struct comp_dict_item_t
 {
     int token;
     char *text;
+    union dataValue value;
     int lineNumber;
-} comp_dict_item_t;
+} comp_dict_item_t,*comp_dict_item_t_p;
 
 typedef struct comp_dict_t
 {
@@ -21,7 +32,8 @@ typedef struct comp_dict_t
 comp_dict_t* dictionary;
 int dict_find_index();
 int dict_find();
-void dict_insert();
+comp_dict_item_t_p dict_insert();
 comp_dict_t* dict_new();
 void dict_free();
 void dict_print();
+
