758545fa4285165e52a0e9a84f9fc72d436d2f8e
fsoller <fsoller@gmail.com>
2013-09-17 03:01:33 -0300

    Update comp_dict.h

--- include/comp_dict.h
+++ include/comp_dict.h
@@ -1,12 +1,20 @@
 /* Projeto de Compiladores 2013-2 - Etapa 0
-   Fernando Soller Mecking
-   Mateus Cardoso da Silva
+Fernando Soller Mecking
+Mateus Cardoso da Silva
 
-   comp_dict.h
-   Estruturas de dados e definições do dicionário
+comp_dict.h
+Estruturas de dados e definições do dicionário
 */
 #ifndef COMP_DICT_H
 #define COMP_DICT_H
+
+#define IKS_SIMBOLO_LITERAL_INT 1
+#define IKS_SIMBOLO_LITERAL_FLOAT 2
+#define IKS_SIMBOLO_LITERAL_CHAR 3
+#define IKS_SIMBOLO_LITERAL_STRING 4
+#define IKS_SIMBOLO_LITERAL_BOOL 5
+#define IKS_SIMBOLO_IDENTIFICADOR 6
+
 union dataValue
 {
     int i;
@@ -17,7 +25,7 @@ union dataValue
 
 typedef struct comp_dict_item_t
 {
-    int token;
+    int type;
     char *text;
     union dataValue value;
     int lineNumber;
