eef1deb9f3c4a52859995b27df88cb9d777a08c5
fsoller <fsoller@gmail.com>
2013-10-03 23:55:22 -0300

    Arquivos para Etapa4

---
+++ src/semantics.c
@@ -0,0 +1,6 @@
+/**
+ * @file semantics.c
+ * @author Arthur Foscarini, Fernando Soller Mecking; Mateus Cardoso da Silva
+ * @date 03 Oct 2013
+ * @brief Arquivo contendo as funções para avaliação semântica
+ */
