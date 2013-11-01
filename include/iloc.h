db3b45f451c3b2c5bda06929d894aad61c425259
MateusCardoso <mateus.c.silva@hotmail.com>
2013-10-31 22:08:37 -0200

    Definicaode iloc.h

---
+++ include/iloc.h
@@ -0,0 +1,52 @@
+/**
+ * Definicao das estruturas necessarias para a geracao de codigo ILOC para a linguagem IKS
+ * */
+#ifndef ILOC_H
+#define ILOC_H
+
+#define ILOC_ADD 		1
+#define ILOC_SUB 		2
+#define ILOC_MULT 		3
+#define ILOC_DIV 		4
+#define ILOC_ADD_I 		5
+#define ILOC_SUB_I 		6
+#define ILOC_R_SUB_I 	7
+#define ILOC_MULT_I 	8
+#define ILOC_DIV_I 		9
+#define ILOC_R_DIV_I 	10
+#define ILOC_LSHIFT 	11
+#define ILOC_LSHIFT_I 	12
+#define ILOC_RSHIFT 	13
+#define ILOC_RSHIFT_I 	14
+#define ILOC_LOAD 		15
+#define ILOC_LOAD_AI	16
+#define ILOC_LOAD_AO	17
+#define ILOC_CLOAD		18
+#define ILOC_CLOAD_AI	19
+#define ILOC_CLOAD_AO	20
+#define ILOC_LOAD_I		21
+#define ILOC_STORE		22
+#define ILOC_STORE_AI	23
+#define ILOC_STORE_AO	24
+#define ILOC_CSTORE		25
+#define ILOC_CSTORE_AI	26
+#define ILOC_CSTORE_AO	27
+#define ILOC_I2I		28
+#define ILOC_C2C		29
+#define ILOC_C2I		30
+#define ILOC_I2I		31
+#define ILOC_CMP_LT		32
+#define ILOC_CMP_LE		33
+#define ILOC_CMP_EQ		34
+#define ILOC_CMP_GE		35
+#define ILOC_CMP_GT		36
+#define ILOC_CMP_NE		37
+#define ILOC_CBR		38
+#define ILOC_JUMP_I		39
+#define ILOC_JUMP		40
+
+
+
+
+
+#endif
